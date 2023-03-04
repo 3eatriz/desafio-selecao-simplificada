41654e801339968880677a711f299f1fb0f9d014
Beatriz <beatrizfreitasti@gmail.com>
2023-03-04 11:11:01 -0300

    DELETE de Pessoa

--- src/main/java/com/desafio/desafio/controllers/PessoaController.java
+++ src/main/java/com/desafio/desafio/controllers/PessoaController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -53,5 +55,12 @@ public class PessoaController {
         pessoa.atualizarInformacoes(dados);
 
         return ResponseEntity.ok(new DadosDetalhamentoPessoa(pessoa));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity desativar(@PathVariable Long id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
